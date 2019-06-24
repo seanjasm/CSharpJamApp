573e864c7f9bc2d2757be0b4e9d2dc5ae54ed14d
seanjasm <seanjasm@gmail.com>
2019-06-24 10:59:57 -0400

    commit to pull

---
+++ CSharpJamApp/Models/IAttack.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpJamApp.Models
+{
+    public interface IAttack
+    {
+        double Attack(UserPlayer player);
+        double Defend(UserPlayer player);
+        double HitProbability(UserPlayer player);
+        double MissedProbability(UserPlayer player);
+    }
+}
